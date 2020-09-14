089a978669da51def9f78e6d270581e4db197690
Ameer Hamza <ameer.hamza6555@yahoo.com>
2020-09-14 17:29:02 +0500

    Added one-to-many association between users and aricles from back-end

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -18,6 +18,7 @@ class ArticlesController < ApplicationController
 
   def create
     @article = Article.new(params.require(:article).permit(:title, :description))
+    @article.user = User.first
     if @article.save
       flash[:success] = "Article saved succesfully"
       redirect_to @article
