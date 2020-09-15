b03b2668c3e6471324a14da3ae4d4c0ce1b97117
Ameer Hamza <ameer.hamza6555@yahoo.com>
2020-09-15 11:24:02 +0500

    New-userform

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   # get 'article', to: 'articles#article'
   root 'pages#home'
   resources :articles
+  get 'signup', to: 'users#new'
+  resources :users, except: [:new] 
 end
